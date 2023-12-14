4f3eeff0edf60f0c15c82e4c76250d574cac0ab9
unknown <aoxoeoxoa@gmail.com>
2023-12-14 10:19:22 +0100

    #windo-location

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,20 +1,26 @@
-import React from "react"
+import React from 'react';
+
+const url = window.location.hostname + ':3003/';
 
 class App extends React.Component {
   state = {
-    name: ""
-  }
+    name: '',
+  };
 
   componentDidMount() {
-    fetch("http://localhost:3003")
-      .then(res => res.json())
-      .then(data => this.setState({ name: data.name }))
+    fetch(`http://${url}`)
+      .then((res) => res.json())
+      .then((data) => this.setState({ name: data.name }));
   }
   render() {
     return (
-      <h1>Hello {this.state.name}!</h1>
-    )
+      <>
+        <h1>Hello {this.state.name}!</h1>
+        <h4>{url}</h4>
+        <h4>{window.location.href}</h4>
+      </>
+    );
   }
 }
 
-export default App
+export default App;
