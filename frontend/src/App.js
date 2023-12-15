844c02d7ab2fca74b4aaec122231e5f107be3900
unknown <aoxoeoxoa@gmail.com>
2023-12-15 15:26:16 +0100

    #10

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
-console.log(process.env.NEXT_PUBLIC_VERCEL_URL);
+
 const App = () => {
   const [name, setName] = useState('');
   const [loading, setLoading] = useState(true); // Aggiunto uno stato per gestire il caricamento
@@ -8,7 +8,7 @@ const App = () => {
   useEffect(() => {
     const fetchData = async () => {
       try {
-        const response = await axios.get(`${process.env.VERCEL_URL}`);
+        const response = await axios.get('https://a01vercel-sever.vercel.app/');
         if (response.status === 200) {
           setName(response.data.name);
         } else {
@@ -26,16 +26,13 @@ const App = () => {
 
   return (
     <>
-      <h1>Coomit 5!</h1>
+      <h1>Coomit 72375</h1>
       {loading ? (
         <p>Loading...</p>
       ) : (
         <>
           {name ? <h1>Hello {name}!</h1> : <p>No data available</p>}
           <h4>{window.location.href}</h4>
-
-          <h4>VERCEL_URL_3</h4>
-          <h4>{process.env.NEXT_PUBLIC_VERCEL_URL}</h4>
         </>
       )}
     </>
--- server/index.js
+++ server/index.js
@@ -4,11 +4,10 @@ const cors = require("cors")
 const port = 3003;
 
 app.use(cors())
-
 app.get("/", function(req, res) {
     res.send({"name": "Jane Doe"}) // Should be json format
   })
-
   app.listen(port, () => {
     console.log(`Server running on port ${port}`);
+   
   });
