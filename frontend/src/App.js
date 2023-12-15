68310509319cc64b6a854e71828498256e7b3a9c
unknown <aoxoeoxoa@gmail.com>
2023-12-15 10:57:18 +0100

    updateaxios  client app.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,26 +1,30 @@
-import React from 'react';
+import React, { useState } from 'react';
+import axios from 'axios';
 
-//const url = window.location.hostname + ':3003/';
+const App = () => {
+  const [name, setName] = useState('');
 
-class App extends React.Component {
-  state = {
-    name: '',
+  const fetchMessage = async () => {
+    try {
+      const response = await axios.get('https://a01vercel-client.vercel.app/');
+      if (response.status === 200) {
+        setName(response.data.name);
+      } else {
+        console.log('non ha avuto successo');
+      }
+    } catch (error) {
+      console.error('Errore durante la richiesta al server:', error);
+    }
   };
+  fetchMessage();
+  return (
+    <>
+      <h1>Coomit!</h1>
+      <h1>Hello {name}!</h1>
 
-  componentDidMount() {
-    fetch(`https://a01vercel-client.vercel.app:3003`)
-      .then((res) => res.json())
-      .then((data) => this.setState({ name: data.name }));
-  }
-  render() {
-    return (
-      <>
-        <h1>Hello {this.state.name}!</h1>
-
-        <h4>{window.location.href}</h4>
-      </>
-    );
-  }
-}
+      <h4>{window.location.href}</h4>
+    </>
+  );
+};
 
 export default App;
