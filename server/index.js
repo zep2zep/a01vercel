c8bfca0ccc6b9abed0b67372e0d6cf6692b8cf51
unknown <aoxoeoxoa@gmail.com>
2023-12-15 14:44:24 +0100

    #8 NEXT_PUBLIC_VERCEL_URL VERCEL_URL

--- server/index.js
+++ server/index.js
@@ -4,9 +4,7 @@ const cors = require("cors")
 const port = 3003;
 
 app.use(cors())
-app.use(cors({
-  origin: 'https://a01vercel-server.vercel.app/'
-}));
+
 app.get("/", function(req, res) {
     res.send({"name": "Jane Doe"}) // Should be json format
   })
