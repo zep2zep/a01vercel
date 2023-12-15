df10595d392f634b0b64c76c6ecc7a81ff122205
unknown <aoxoeoxoa@gmail.com>
2023-12-15 16:25:26 +0100

    #13

--- server/index.js
+++ server/index.js
@@ -3,7 +3,12 @@ const app = express();
 const cors = require("cors")
 const port = 3003;
 
-app.use(cors())
+const corsOptions = {
+  origin: 'https://a01vercel-server.vercel.app/', // Sostituisci con l'URL del tuo frontend
+  optionsSuccessStatus: 200 // Alcune versioni di CORS richiedono questo parametro
+};
+
+app.use(cors(corsOptions));
 app.get("/", function(req, res) {
     res.send({"name": "Jane Doe dal backend"}) // Should be json format
   })
