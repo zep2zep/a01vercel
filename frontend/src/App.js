6f108a72ad71e21ea410d25699d9d7018a640589
unknown <aoxoeoxoa@gmail.com>
2023-12-15 11:05:21 +0100

    upda name  client app.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,27 +1,32 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 
 const App = () => {
   const [name, setName] = useState('');
 
-  const fetchMessage = async () => {
-    try {
-      const response = await axios.get('https://a01vercel-client.vercel.app/');
-      if (response.status === 200) {
-        setName(response.data.name);
-      } else {
-        console.log('non ha avuto successo');
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await axios.get(
+          'https://a01vercel-client.vercel.app/'
+        );
+        if (response.status === 200) {
+          setName(response.data.name);
+        } else {
+          console.log('La richiesta non ha avuto successo');
+        }
+      } catch (error) {
+        console.error('Errore durante la richiesta al server:', error);
       }
-    } catch (error) {
-      console.error('Errore durante la richiesta al server:', error);
-    }
-  };
-  fetchMessage();
+    };
+
+    fetchData(); // Effettua la richiesta al backend quando il componente viene montato
+  }, []); // Assicura che useEffect venga eseguito solo una volta al caricamento del componente
+
   return (
     <>
       <h1>Coomit!</h1>
-      <h1>Hello {name}!</h1>
-
+      {name ? <h1>Hello {name}!</h1> : <p>Loading...</p>}
       <h4>{window.location.href}</h4>
     </>
   );
